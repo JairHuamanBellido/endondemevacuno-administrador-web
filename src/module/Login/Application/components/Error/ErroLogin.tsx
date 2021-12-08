f5dce1bd0d95ee075ee8082af4bb401956841cf8
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-12-08 12:28:38 -0500

    Remove comments and update variable

--- src/module/Login/Application/components/Error/ErroLogin.tsx
+++ src/module/Login/Application/components/Error/ErroLogin.tsx
@@ -5,12 +5,10 @@ interface IProps {
   message: HttpError | null;
 }
 export default function LoginError(props: IProps) {
-  const nsg = props.message?.response?.data.message;
-  // console.log(props.message.response.data.message);
+  const errorMessage = props.message?.response?.data.message;
   return (
     <div data-testid="error" className="border-radius-4 container-error">
-      <p>{nsg}</p>
-      {/* <p>Credenciales incorrectas</p> */}
+      <p>{errorMessage}</p>
     </div>
   );
 }
