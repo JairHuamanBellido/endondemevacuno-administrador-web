35d36e6052f9cd2ed86ad2a7546b9f01489e1433
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-12-08 12:28:05 -0500

    Add types for authentication api response

--- src/module/Login/Infrastructure/HttpRestApiAuthentication.ts
+++ src/module/Login/Infrastructure/HttpRestApiAuthentication.ts
@@ -1,9 +1,15 @@
 import { HttpRestApi } from "core/api/HttpRestApi";
 import { AuthenticationRequest } from "../Domain/entity/AuthenticationRequest.interface";
+import { HttpRestApiAuthenticationResponse } from "./model/HttpApiRestAuthenticationResponse.model";
 
 export class HttpRestApiAuthentication {
-  public static async login(payload: AuthenticationRequest) {
-    const { data } = await HttpRestApi.post("/auth", payload);
+  public static async login(
+    payload: AuthenticationRequest
+  ): Promise<HttpRestApiAuthenticationResponse> {
+    const { data } = await HttpRestApi.post<HttpRestApiAuthenticationResponse>(
+      "/auth",
+      payload
+    );
 
     return data;
   }
